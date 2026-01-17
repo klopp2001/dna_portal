9b0a77472a1048540b3b45c49bdb29ac939980e1
klopp2001 <146027001+klopp2001@users.noreply.github.com>
2026-01-17 19:46:42 +0300

    ts textarea bugfix

--- app/components/CommentModal.tsx
+++ app/components/CommentModal.tsx
@@ -6,14 +6,18 @@ interface CommentModalProps {
 }
 
 const CommentModal = ({ setComent: setComment }: CommentModalProps) => {
-  const textareaRef = useRef(null)
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null)
   return (
     <div className="fixed inset-0 w-full h-full z-50 ">
       <div className="w-full h-full bg-gray-400 opacity-45"></div>
       <div className="absolute top-1/12 px-4 left-14 bg-white ">
         <p>Введите свой комментарий:</p>
         <textarea className="h-80 w-full my-2" ref={textareaRef}></textarea>
-        <button onClick={() => setComment(textareaRef.current.value)}>
+        <button
+          onClick={() => {
+            if (textareaRef.current) setComment(textareaRef.current.value)
+          }}
+        >
           Отправить
         </button>
       </div>
